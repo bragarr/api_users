9f426664bb0237742e2ad86dedf0ba90253a6975
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2023-01-03 23:58:24 -0400

    feat: Criando servidor

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log("Servidor Ativo!");
+})
